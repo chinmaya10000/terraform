d3d11ec9ed23bb9e8161e5c49640564e5bfc1926
chinmaya <chinmaya@gmail.com>
2024-05-08 15:44:55 +0530

    initial commit

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,3 @@
-output "aws_ami_id" {
-  value = data.aws_ami.latest-ubuntu.id
-}
-
-resource "aws_key_pair" "ssh-key" {
-  key_name = "server-key"
-  public_key = file(var.public_key_location)
+output "ec2_public_ip" {
+  value = module.myapp-srver.instance.public_ip
 }
--- variables.tf
+++ variables.tf
@@ -5,3 +5,4 @@ variable my_ip {}
 variable env_prefix {}
 variable instance_type {}
 variable public_key_location {}
+variable image_name {}
