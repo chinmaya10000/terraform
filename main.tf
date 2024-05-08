633a5ac7a079d104b3189ec8208a4d79c2a25c16
chinmaya10000 <chinmayapradhan10000@gmail.com>
2024-05-08 10:21:40 +0000

    modularize tf project

--- main.tf
+++ main.tf
@@ -5,27 +5,27 @@ provider "aws" {
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
-    Name: "${var.env_prefix}-vpc"
+    Name : "${var.env_prefix}-vpc"
   }
 }
 
 module "myapp-subnet" {
-  source = "./modules/subnet"
-  vpc_id = aws_vpc.myapp-vpc.id
-  subnet_cidr_block = var.subnet_cidr_block
-  avail_zone = var.avail_zone
-  env_prefix = var.env_prefix
+  source                 = "./modules/subnet"
+  vpc_id                 = aws_vpc.myapp-vpc.id
+  subnet_cidr_block      = var.subnet_cidr_block
+  avail_zone             = var.avail_zone
+  env_prefix             = var.env_prefix
   default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
 }
 
 module "myapp-srver" {
-  source = "./modules/webserver"
-  vpc_id = aws_vpc.myapp-vpc.id
-  my_ip = var.my_ip
-  env_prefix = var.env_prefix
-  image_name = var.image_name
-  instance_type = var.instance_type
-  subnet_id = module.myapp-subnet.subnet.id
+  source              = "./modules/webserver"
+  vpc_id              = aws_vpc.myapp-vpc.id
+  my_ip               = var.my_ip
+  env_prefix          = var.env_prefix
+  image_name          = var.image_name
+  instance_type       = var.instance_type
+  subnet_id           = module.myapp-subnet.subnet.id
   public_key_location = var.public_key_location
-  avail_zone = var.avail_zone
+  avail_zone          = var.avail_zone
 }
