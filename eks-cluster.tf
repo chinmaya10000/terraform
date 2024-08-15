4c9552900b01261846348d28319fc837635c9d07
chinmaya <chinmaya@gmail.com>
2024-08-15 23:28:53 +0530

    Authenticate with k8s

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,3 +1,18 @@
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.23.0"
