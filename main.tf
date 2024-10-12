20fb2adbffe622a7eaf77ecf68b4682e870841f7
chinmaya <chinmaya@gmail.com>
2024-10-12 19:13:19 +0530

    initial commit

--- main.tf
+++ main.tf
@@ -2,120 +2,116 @@ provider "aws" {
   region = "us-east-2"
 }
 
+# Data source for availability zones
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 variable "vpc_cidr_block" {}
-variable "subnet_cidr_block" {}
-variable "avail_zone" {}
-variable "my_ip" {}
 variable "env_prefix" {}
-variable "instance_type" {}
-variable "public_key_location" {}
 
-resource "aws_vpc" "myapp-vpc" {
+# Create VPC
+resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr_block
+
   tags = {
-    Name : "${var.env_prefix}-vpc"
+    Name = "${var.env_prefix}-vpc"
   }
 }
 
-resource "aws_subnet" "myapp-subnet" {
-  vpc_id            = aws_vpc.myapp-vpc.id
-  cidr_block        = var.subnet_cidr_block
-  availability_zone = var.avail_zone
+# Create 2 Public Subnet
+resource "aws_subnet" "public" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.${count.index + 1}.0/24"
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+
+  count = 2
+
   tags = {
-    Name : "${var.env_prefix}-subnet"
+    Name = "${var.env_prefix}-public-subnet-${count.index + 1}"
   }
 }
 
-resource "aws_internet_gateway" "myapp-igw" {
-  vpc_id = aws_vpc.myapp-vpc.id
+# Create 2 Private Subnet
+resource "aws_subnet" "private" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.${count.index + 101}.0/24"
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  
+
+  count = 2
+
   tags = {
-    Name : "${var.env_prefix}-igw"
+    Name = "${var.env_prefix}-private-subnet-${count.index + 1}"
   }
 }
 
-resource "aws_default_route_table" "myapp-rtb" {
-  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+# Create Internet Gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.env_prefix}-igw"
+  }
+}
+
+# Create Public Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.myapp-igw.id
+    gateway_id = aws_internet_gateway.main.id
   }
 
   tags = {
-    Name : "${var.env_prefix}-main-rtb"
+    Name = "${var.env_prefix}-public-route-table"
   }
 }
 
-resource "aws_default_security_group" "default-sg" {
-  vpc_id = aws_vpc.myapp-vpc.id
+# Associate Public Subnets with Public Route Table
+resource "aws_route_table_association" "public" {
+  count = 2
+  subnet_id = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.my_ip]
-  }
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-    prefix_list_ids = []
-  }
+# Create Elastic IP for NAT Gateway
+resource "aws_eip" "nat" {
+  domain = "vpc"
 
   tags = {
-    Name : "${var.env_prefix}-default-sg"
+    Name = "${var.env_prefix}-nat-eip"
   }
 }
 
-data "aws_ami" "latest-ubuntu" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
-output "aws_ami_id" {
-  value = data.aws_ami.latest-ubuntu.id
-}
-
-resource "aws_key_pair" "ssh-key" {
-  key_name   = "server-key"
-  public_key = file(var.public_key_location)
-}
-
-resource "aws_instance" "myapp-server" {
-  ami           = data.aws_ami.latest-ubuntu.id
-  instance_type = var.instance_type
-
-  subnet_id              = aws_subnet.myapp-subnet.id
-  vpc_security_group_ids = [aws_default_security_group.default-sg.id]
-  availability_zone      = var.avail_zone
-
-  associate_public_ip_address = true
-  key_name                    = aws_key_pair.ssh-key.key_name
-
-  user_data = file("entry-script.sh")
+# Create NAT Gateway
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id = aws_subnet.public[0].id
 
   tags = {
-    Name : "${var.env_prefix}-server"
+    Name = "${var.env_prefix}-nat-gateway"
   }
 }
 
-output "ec2_public_ip" {
-  value = aws_instance.myapp-server.public_ip
-  # terraform state show aws_instance.myapp-server
+# Create Private Route Table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+  tags = {
+    Name = "${var.env_prefix}-private-route-table"
+  } 
+}
+
+# Associate Private Subnets with Private Route Table
+resource "aws_route_table_association" "private" {
+  count = 2
+  subnet_id = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
 }
