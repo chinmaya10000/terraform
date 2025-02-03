a531df6f70f2643a6316ab968203318fc440a414
chinmaya <chinmaya@gmail.com>
2025-02-03 11:55:16 +0530

    initiall commit

--- main.tf
+++ main.tf
@@ -8,12 +8,12 @@ variable "avail_zone" {}
 variable "my_ip" {}
 variable "env_prefix" {}
 variable "instance_type" {}
-variable "public_key_location" {}
+# variable "public_key_location" {}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
-    Name : "${var.env_prefix}-vpc"
+    Name = "${var.env_prefix}-vpc"
   }
 }
 
@@ -22,14 +22,14 @@ resource "aws_subnet" "myapp-subnet" {
   cidr_block        = var.subnet_cidr_block
   availability_zone = var.avail_zone
   tags = {
-    Name : "${var.env_prefix}-subnet"
+    Name = "${var.env_prefix}-subnet"
   }
 }
 
 resource "aws_internet_gateway" "myapp-igw" {
   vpc_id = aws_vpc.myapp-vpc.id
   tags = {
-    Name : "${var.env_prefix}-igw"
+    Name = "${var.env_prefix}-igw"
   }
 }
 
@@ -42,7 +42,7 @@ resource "aws_default_route_table" "myapp-rtb" {
   }
 
   tags = {
-    Name : "${var.env_prefix}-main-rtb"
+    Name = "${var.env_prefix}-main-rtb"
   }
 }
 
@@ -70,35 +70,35 @@ resource "aws_default_security_group" "default-sg" {
   }
 
   tags = {
-    Name : "${var.env_prefix}-default-sg"
+    Name = "${var.env_prefix}-default-sg"
   }
 }
 
-data "aws_ami" "latest-ubuntu" {
-  most_recent = true
-  owners      = ["amazon"]
+# data "aws_ami" "latest-ubuntu" {
+#   most_recent = true
+#   owners      = ["amazon"]
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+#   }
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
+# }
 
 output "aws_ami_id" {
   value = data.aws_ami.latest-ubuntu.id
 }
 
-resource "aws_key_pair" "ssh-key" {
-  key_name   = "server-key"
-  public_key = file(var.public_key_location)
-}
+# resource "aws_key_pair" "ssh-key" {
+#   key_name   = "server-key"
+#   public_key = file(var.public_key_location)
+# }
 
 resource "aws_instance" "myapp-server" {
-  ami           = data.aws_ami.latest-ubuntu.id
+  ami           = ami-0cb91c7de36eed2cb
   instance_type = var.instance_type
 
   subnet_id              = aws_subnet.myapp-subnet.id
@@ -106,12 +106,12 @@ resource "aws_instance" "myapp-server" {
   availability_zone      = var.avail_zone
 
   associate_public_ip_address = true
-  key_name                    = aws_key_pair.ssh-key.key_name
+  key_name                    = "eks-iac-key"
 
   user_data = file("entry-script.sh")
 
   tags = {
-    Name : "${var.env_prefix}-server"
+    Name = "${var.env_prefix}-server"
   }
 }
 
